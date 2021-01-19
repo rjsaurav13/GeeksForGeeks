fe33a88b1e34c28d7ee9450fd6e76d6a1005c9a0
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-20 00:13:31 +0530

    n

--- Ascii_Sum.cpp
+++ Ascii_Sum.cpp
@@ -6,22 +6,22 @@ int main()
     cin>>tcase;
     for(int i =0;i<tcase;i++){
         string s;
-        int sum=0;
+        int sum=0,pro=0;
         cin>>s;
         for(int j=0;j<s.length();j++){
             sum=sum+int(s[j]);
         }
-        for(int i=1;i<12;i++) {
-            if(i*i==sum){
+        for(int i=0;i<sizeof(arr);i++){
 
-                cout<<"\n1\n";
+            if(arr[i]==sum){
+                cout<<"1\n";
                 break;
             }
-            else{
-                cout<<"\n0\n";
-                break;
-            }
+        else{
+            cout<<"0\n";
+            break;
         }
+    }
     }
 
 
