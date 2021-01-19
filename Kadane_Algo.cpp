6598685525091ebaebc047c2ae70c098e4d6dd33
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-20 00:12:36 +0530

    Create Kadane_Algo.cpp

---
+++ Kadane_Algo.cpp
@@ -0,0 +1,44 @@
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+arr[i];
+    }
+    if(sum>0){
+        return sum;
+    }
+    return -1;
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+
+        cin>>n; //input size of array
+
+        int a[n];
+
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
