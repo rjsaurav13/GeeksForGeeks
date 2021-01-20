d3b8e877519e989f2040f962df3bcbbef6ee23c2
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-21 00:36:09 +0530

    Update Smallest2nd.cpp

--- Smallest2nd.cpp
+++ Smallest2nd.cpp
@@ -8,10 +8,7 @@ int main()
     for(int i =0;i<tcase;i++){
         cin>>len;
         int arr[len];
-        for(int i=0;i<len;i++){
-            cin>>arr[i];
-        }
-        for(int i=0;i<len;i++){
+        for(int i=0;i<len;i++){for(int i=0;i<len;i++){
             for(int j=i+1;j<len;j++){
                 if(arr[i]>arr[j]){
                         temp=arr[i];
@@ -21,6 +18,9 @@ int main()
                 }
             }
         }
+            cin>>arr[i];
+        }
+
         for(int k=0;k<len;k++){
             if(len<2||arr[0]==arr[k]){
                 cout<<"-1\n";
