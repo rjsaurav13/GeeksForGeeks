8d5653d549765b2e607f25c9f320062ff887cd9c
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-22 00:22:28 +0530

    Create DiviBy4.cpp

---
+++ DiviBy4.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+class Solution{
+public:
+
+	int divisibleBy4 (string N)
+	{
+	    int n = stoi(N);
+	    cout<<n%4;
+
+	}
+};
+
+// { Driver Code Starts.
+
+int32_t main()
+{
+	int t; cin >> t;
+	while (t--)
+	{
+		string s; cin >> s;
+		Solution ob;
+		cout << ob.divisibleBy4 (s) << endl;
+	}
+}
+
+// Contributed By: Pranay Bansal
+  // } Driver Code Ends
+
