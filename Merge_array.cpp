7bbfb606af7ea1b13fcc9699632ef1aa477057c0
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-25 00:45:19 +0530

    Update Merge_array.cpp

--- Merge_array.cpp
+++ Merge_array.cpp
@@ -8,19 +8,41 @@ using namespace std;
 
 void merge(int arr1[], int arr2[], int n, int m)
 {
-    int k=m+n;
-    int arr3[k];
-    for(int i=0;i<n;i++){
-        arr3[i]=arr1[i];
-    }
-    for(int j=0;j<m;j++){
-        arr3[j+m]=arr2[j];
-    }
-    for(int i=0;i<k;i++){
-        cout<<arr3[i]<<" ";
-    }
+    int o = n + m;
+		int arr3[o];
+		for (int i=0;i<n;i++) {
+
+			arr3[i]=arr1[i];
+		}
+
+		for (int i=0;i<m;i++) {
+
+			arr3[n+i]=arr2[i];
+		}
 
 
+		int temp;
+		for(int i=0;i<o;i++){
+            for(int j=i+1;j<o;j++){
+                if(arr3[i]>arr3[j]){
+                        temp=arr3[i];
+                        arr3[i]=arr3[j];
+                        arr3[j]=temp;
+
+                }
+            }
+        }
+
+		for (int i=0;i<n;i++) {
+            cout<<arr1[i]<<" ";
+            //arr1[i]=arr3[i];
+            //arr2[n+i]=arr3[i];
+		}
+
+        for (int e=0;e<n;e++) {
+
+			//cout<<arr2[e]<<" ";
+		}
 }
 
 
@@ -48,14 +70,15 @@ int main()
 
 	    merge(arr1, arr2, n, m);
 
-        for (int i = 0; i < n; i++)
-            printf("%d ", arr1[i]);
+        //for (int i = 0; i < n; i++)
+          //  printf("%d ", arr1[i]);
+            //cout<<"---------------------------------";
 
 
-	    for (int i = 0; i < m; i++)
-		    printf("%d ", arr2[i]);
+	  //  for (int i = 0; i < m; i++)
+		//    printf("%d ", arr2[i]);
 
-	    cout<<endl;
+	   // cout<<endl;
 	}
 
 	return 0;
