046b0c11334d14b93b948d97d2130d97f5839def
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-26 12:12:21 +0530

    Create Adjacent_char.java

---
+++ geekforgeeks/src/geekforgeeks/Adjacent_char.java
@@ -0,0 +1,27 @@
+package geekforgeeks;
+
+import java.util.Scanner;
+
+public class Adjacent_char {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		String s = scanner.next();
+		int arr[] = new int[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i + 1; j < s.length(); j++) {
+				if (s.charAt(i) == s.charAt(j)) {
+					arr[i] = i;
+					
+				}
+			}
+		}
+		for (int i = 0; i < s.length(); i++) {
+			if (i == arr[i]) {
+				System.out.println(s.charAt(i));
+			}
+
+		}
+
+	}
+}
