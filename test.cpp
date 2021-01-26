8dc79ca93efb70cda879509d4d934c5cfae9e217
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-27 01:10:03 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
+for(int i=1;i<=100;i++){
+            int temp1,count=0,temp=i;
+            while(i!=0){
 
-    int num;
-    cin>>num;
-    int temp,num2=0;
-    while(num>0)
-    {
-        num2 = num2*10 + num%10;
-        num = num/10;
-    }
-    cout<<num<<" "<<num2;
+                temp1=temp%10;
+                if(temp1==9){
+                    break;
+                }
+                else{
+                    count++;
+                }
+
+            }
+            cout<<count;
+        }
+
+
 
 return 0;
 }
