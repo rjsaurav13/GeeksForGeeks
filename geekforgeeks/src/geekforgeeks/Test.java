924546e7699ec782df91d56ea18d1a67819d6691
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-01-29 00:41:31 +0530

    Update Test.java

--- geekforgeeks/src/geekforgeeks/Test.java
+++ geekforgeeks/src/geekforgeeks/Test.java
@@ -4,46 +4,11 @@ import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int arr1[] = new int[n];
-		int arr2[] = new int[m];
-		
-		for (int i = 0; i < n; i++) {
-			arr1[i] = sc.nextInt();
+		String s1 = "java.string.split.method.by.javatpoint";
+		String[] words = s1.split(" ");// splits the string based on whitespace
+		// using java foreach loop to print elements of string array
+		for (String w : words) {
+			System.out.println(w);
 		}
-
-		for (int i = 0; i < m; i++) {
-			arr2[i] = sc.nextInt();
-		}
-		int o = n + m;
-		int arr3[] = new int[o];
-		for (int i= 0;i<n;i++) {
-
-			arr3[i]=arr1[i];
-		}
-
-		for (int i=0;i<m;i++) {
-
-			arr3[n+i]=arr2[i];
-		}
-
-		
-		int temp;
-		for(int i=0;i<o;i++) {
-			for(int j=i+1;j<o;j++) {
-				if(arr3[i]>arr3[j]) {
-					temp=arr3[i];
-					arr3[i]=arr3[j];
-					arr3[j]=arr3[i];
-				}
-			}
-		}
-		for (int e=0;e<o;e++) {
-
-			System.out.println(arr3[e]);
-		}
-
 	}
 }
