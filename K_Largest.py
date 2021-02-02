9e70b31d7a4165f66132820e3a43648c31317168
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-02 20:00:48 +0530

    Update K_Largest.py

--- K_Largest.py
+++ K_Largest.py
@@ -1,9 +1,30 @@
-fruits = [1,2,3,4,5]
-fruits.sort(reverse=True)
+# User function Template for python3
+class Solution:
 
-cars = [None] * 1
-x=0
-while x!=3:
-	cars.append(x)
-	x=x+1
-print(cars)
+    def kLargest(self, arr, n, k):
+        arr.sort(reverse=True)
+        arr2 = [None] * k
+        for i in range(0, k):
+            arr2[i] = arr[i]
+
+    return arr2
+
+
+# {
+#  Driver Code Starts
+# Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.kLargest(arr, n, k)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
