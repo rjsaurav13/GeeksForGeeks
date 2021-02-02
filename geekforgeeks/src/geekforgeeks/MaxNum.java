a25dd8c93b1d8233efe594f6209140e8e1b3358b
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-02 20:00:36 +0530

    Update MaxNum.java

--- geekforgeeks/src/geekforgeeks/MaxNum.java
+++ geekforgeeks/src/geekforgeeks/MaxNum.java
@@ -5,36 +5,15 @@ import java.util.Scanner;
 public class MaxNum {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String num = scanner.next();
-		int len = num.length();
-		int k = Integer.parseInt(num);
-		int[] arr = new int[len];
-		for (int i = 0; i < len; i++) {
-			int temp;
-			while (k != 0) {
-				temp = k % 10;
-				arr[i] = temp;
-				k = k / 10;
-			}
-
+		int num = scanner.nextInt();
+		int temp, num2 = 0;
+		while (num != 0) {
+			temp = num % 10;
+			num2 = num2 + temp * 1;
+			num = num / 10;
 		}
-		int temp1;
-		for (int i = 0; i < len; i++) {
-			for (int j = i + 1; j < len; j++) {
-				if (arr[i] > arr[j]) {
-					temp1 = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp1;
-
-				}
-			}
-		}
-		int t=0;
-		for (int i = 0; i < len; i++) {
-			t=t*arr[i];
-		}
-		String s=String.valueOf(t);
-		System.out.println(s);
+		System.out.println(num2);
 		scanner.close();
+
 	}
 }
