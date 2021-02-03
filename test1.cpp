925c83086a0b2e321f443563fd56760ee89856aa
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-03 19:40:29 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -2,9 +2,22 @@
 using namespace std;
 int main()
 {
-for(int i=1;i<=20;i++){
-    int n=i*i;
-    cout<<n<<" ";
+int N;
+cin>>N;
+int count=0;
+for(int i=1;i<=N;i++){
+
+    int number=i;
+    while (number > 0)
+    {
+        if(number % 10 == 2)
+            count++;
+
+        number = number / 10;
+    }
+
 }
+  cout<<count;
+
 return 0;
 }
