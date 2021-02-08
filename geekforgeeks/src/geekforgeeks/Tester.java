7024a11b1b0d17ca607714cd6bfbd7b72f048346
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-09 01:05:58 +0530

    Update Tester.java

--- geekforgeeks/src/geekforgeeks/Tester.java
+++ geekforgeeks/src/geekforgeeks/Tester.java
@@ -2,9 +2,18 @@ package geekforgeeks;
 
 public class Tester {
 	public static void main(String[] args) {
-		int a=10;
-		int d = a % 7;
-		System.out.println(d);
-	}
+		int n = 100;
 
+		if (n < 10) {
+			System.out.println(n);
+		} else {
+			int lnum = n % 10;
+			while (n > 10) {
+				n = n / 10;
+			}
+			int num=lnum + n;
+			System.out.println(num);
+		}
+
+	}
 }
