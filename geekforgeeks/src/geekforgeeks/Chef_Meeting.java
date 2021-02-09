5a95099e08bb45b00cbabdd1cf2a93b5e1576ea0
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-10 00:42:44 +0530

    Create Chef_Meeting.java

---
+++ geekforgeeks/src/geekforgeeks/Chef_Meeting.java
@@ -0,0 +1,13 @@
+package geekforgeeks;
+
+import java.util.Scanner;
+
+public class Chef_Meeting {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String time=scanner.next();
+		String time2=scanner.next();
+		System.out.println(time+" "+time2);
+		scanner.close();
+	}
+}
