b1e51944bfd9ca332d13eebdb0a7df2c33a2f6fc
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-02-11 00:20:16 +0530

    Update Nth_NaturaL_Num.cpp

--- Nth_NaturaL_Num.cpp
+++ Nth_NaturaL_Num.cpp
@@ -1,1 +1,41 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+    long long N;
+
+    cin >> N;
+    vector<long long> num;
+
+         for(long long j=0;j<=N*N;j++){
+            long long temp=j;
+            int t=1;
+            while(temp>0)
+            {
+
+            if(temp%10==9){
+                    t=0;
+                break;
+                }
+                temp=temp/10;
+            }
+            if(t==1)
+            {
+                num.push_back(j);
+
+            }
+         }
+
+
+
+
+
+    cout<<num[N];
+
+
+return 0;
+}
